2dd03854dd79355aadb4c436463966d09421e73a
SilasTheCoder <39573757+SilasTheCoder@users.noreply.github.com>
2020-02-06 16:22:48 -0600

    Update data.py

--- data.py
+++ data.py
@@ -1,1 +1,6 @@
-print ("Silas has 1000000 dollars")
+print ("Credit Card Data Downloaded")
+print ("Who's class number do you want to check the balance of?")
+inputuser = int(input(" "))
+if inputuser == 1:
+  money = ("0 dollars")
+  print ("Avie has", money)
